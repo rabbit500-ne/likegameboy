2d5c2960f0ff562e83e0c1cf58c1cc8cd2679636
rabbit500-ne <62441910+rabbit500-ne@users.noreply.github.com>
2022-03-14 07:57:17 +0900

    fix

--- src/blend.py
+++ src/blend.py
@@ -1,3 +1,7 @@
+"""
+
+"""
+
 import numpy as np
 import cv2
 
@@ -17,5 +21,29 @@ def backimg(width, height):
     return np.full((height, width, 3), BACK_PAD_COLOR, dtype=np.uint8)
 
 def resize(img, recio_w, recio_h):
-	(w,h,d) = img.shape
-	return cv2.resize(img , (int(w*recio_w), int(h*recio_h)))
+	shape = img.shape
+	return cv2.resize(img , (int(shape[0]*recio_w), int(shape[1]*recio_h)))
+
+def get_gradient_2d(start, stop, width, height, is_horizontal):
+    if is_horizontal:
+        return np.tile(np.linspace(start, stop, width), (height, 1))
+    else:
+        return np.tile(np.linspace(start, stop, height), (width, 1)).T
+
+def get_gradient_3d(width, height, start_list, stop_list, is_horizontal_list):
+    result = np.zeros((height, width, len(start_list)), dtype=np.float)
+
+    for i, (start, stop, is_horizontal) in enumerate(zip(start_list, stop_list, is_horizontal_list)):
+        result[:, :, i] = get_gradient_2d(start, stop, width, height, is_horizontal)
+
+    return result
+
+def alfa_blend(base, layer, ratio):
+    print(f"base.shape{base.shape}")
+    if ratio < 0 or ratio > 1:
+        raise Exception()
+    ret = base.copy()
+    masked = np.where((layer <= [251,251,251]).all(axis=2))
+    ret[masked] = ret[masked] * (1-ratio) + layer[masked] * ratio
+    print(f"ret.shape{ret.shape}")
+    return ret
